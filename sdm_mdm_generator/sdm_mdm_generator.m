e59bbaf8ca65c4d206a50cee61ad67efe1b74e92
Andrea Gerardo Russo <35601556+andreagrusso@users.noreply.github.com>
2020-03-11 16:38:12 +0100

    Update sdm_mdm_generator.m
    Adjustment of the path (input/output), elimination of the semantic distance and introduction of the skipbogram

--- sdm_mdm_generator/sdm_mdm_generator.m
+++ sdm_mdm_generator/sdm_mdm_generator.m
@@ -3,10 +3,11 @@ close all; close all hidden;
 clc
 
 %% Raw data loading
-input_dir = 'new_surprisal';
 
-timestamps_struct = load([input_dir,'/timestamps.mat']);
-surp_data = dlmread([input_dir,'/surprisal_data_12Jul19.txt']);
+
+timestamps_struct = load('timestamps.mat');
+%file_data = uigetfile('*.txt')
+surp_data = dlmread('../output/surprisal_data_2020-03-11.txt');
 
 timestamps = timestamps_struct.timestamps;
 durations = timestamps_struct.durations;
@@ -14,13 +15,10 @@ freq = timestamps_struct.freq;
 sem_surp = surp_data(:,1);
 lex_surp = surp_data(:,2);
 
-sem_surp_noFW = surp_data(:,3);
-prob_cs = surp_data(:,4);
-sem_dist = surp_data(:,5);
 
 %% Path
 
-outdir = 'D:\protocolli_ruggi\natexp\analysis\vtc_smooth';
+outdir = 'C:\Users\andre\_NeuroImaging\scripts\fMRIandSurprisal4Italian\BVanalysis';
 
 
 %% Acoustic predictors
@@ -38,12 +36,9 @@ y_bw = [zeros(audio_start,1);y_bw;zeros(audio_end-audio_start-length(y_bw),1)];
 
 %% Lingusitic data
 
-ls = zscore(-log10(lex_surp)); %lexical
+ls = zscore(lex_surp); %lexical
 
-ss = zscore(-log10(sem_surp)); %semantic
-
-snf = sem_surp_noFW; %semantic with 0 value for function words
-snf(snf>0) = zscore(-log10(snf(snf>0)));
+ss = zscore(sem_surp); %semantic
 
 fr = zscore(log10(freq/222153649)); %log of the relative frequency
 %fr = zscore(log10(freq)); %log of the relative frequency
@@ -51,11 +46,7 @@ fr = zscore(log10(freq/222153649)); %log of the relative frequency
 dr = zscore(durations);
 
 
-sp_min = min(prob_cs(prob_cs>0));
-prob_cs(prob_cs==0) = sp_min;
-sp = zscore(-log10(prob_cs));
 
-sd = zscore(sem_dist);
 
 disp(corrcoef(ss,ls))
 
@@ -75,17 +66,13 @@ hrf = n.hrf('twogamma',dt,5,15,6,0,1,1,[0,tt(end)],0,'area');
 fw_data.ss = ss;
 fw_data.ls = ls;
 fw_data.fr = fr;
-fw_data.sp = sp;
-fw_data.sd = sd;
 fw_data.env = y_fw;
 fw_data.dr = dr;
 fw_data.timestamps = timestamps;
 fw_data.durations = durations;
 
-[fw_des_full,fw_des_sem,fw_des_lex,fw_des_probsem, fw_des_semdist] = create_preds(hrf,tt,t,fw_data,fss);
+[fw_des_full,fw_des_sem,fw_des_lex] = create_preds(hrf,tt,t,fw_data,fss);
 
-% [Q_fw,R_fw] = gsog(fw_des_lex);
-% fw_des_lex = Q_fw;
 
 
 
@@ -100,60 +87,46 @@ end
 bw_fr = flipud(fr);
 bw_ss = flipud(ss);
 bw_ls = flipud(ls);
-bw_sp = flipud(sp);
-bw_sd = flipud(sd);
 bw_dr = flipud(dr);
 
 bw_data.ss = bw_ss;
 bw_data.ls = bw_ls;
 bw_data.fr = bw_fr;
-bw_data.sp = bw_sp;
-bw_data.sd = bw_sd;
 bw_data.dr = bw_dr;
 bw_data.env = y_bw;
 bw_data.timestamps = timestamps_bw;
 bw_data.durations = durations_bw;
 
 
-[bw_des_full,bw_des_sem,bw_des_lex,bw_des_probsem,bw_des_semdist]= create_preds(hrf,tt,t,bw_data,fss);
+[bw_des_full,bw_des_sem,bw_des_lex]= create_preds(hrf,tt,t,bw_data,fss);
 
 
 
 full_list_names = {'Sem','InvSem','LS','InvLS','Freq','InvFreq','Dur','InvDur','Env','InvEnv','Costant'};
 sem_list_names = {'Sem','InvSem','Freq','InvFreq','Dur','InvDur','Env','InvEnv','Costant'};
 lex_list_names = {'LS','InvLS','Freq','InvFreq','Dur','InvDur','Env','InvEnv','Costant'};
-prob_list_names = {'LogSemProb','InvLogSemProb','Freq','InvFreq','Dur','InvDur','Env','InvEnv','Costant'};
-semdist_list_names = {'SemDist','InvSemDist','Freq','InvFreq','Dur','InvDur','Env','InvEnv','Costant'};
 
 %fw SDM
 create_sdm('fw_env.sdm','fw_full.sdm',outdir,fw_des_full,full_list_names,'fw')
 create_sdm('fw_env.sdm','fw_sem.sdm',outdir,fw_des_sem,sem_list_names,'fw')
 create_sdm('fw_env.sdm','fw_lex.sdm',outdir,fw_des_lex,lex_list_names,'fw')
-create_sdm('fw_env.sdm','fw_probsem.sdm',outdir,fw_des_probsem,prob_list_names,'fw')
-create_sdm('fw_env.sdm','fw_semdist.sdm',outdir,fw_des_semdist,semdist_list_names,'fw')
 
 
 
 create_sdm_noenv('fw_env.sdm','fw_full_noenv.sdm',outdir,fw_des_full,full_list_names,'fw')
 create_sdm_noenv('fw_env.sdm','fw_sem_noenv.sdm',outdir,fw_des_sem,sem_list_names,'fw')
 create_sdm_noenv('fw_env.sdm','fw_lex_noenv.sdm',outdir,fw_des_lex,lex_list_names,'fw')
-create_sdm_noenv('fw_env.sdm','fw_probsem_noenv.sdm',outdir,fw_des_probsem,prob_list_names,'fw')
-create_sdm_noenv('fw_env.sdm','fw_semdist_noenv.sdm',outdir,fw_des_semdist,semdist_list_names,'fw')
 
 
 %bw SDM
 create_sdm('bw_env.sdm','bw_full.sdm',outdir,bw_des_full,full_list_names,'bw')
 create_sdm('bw_env.sdm','bw_sem.sdm',outdir,bw_des_sem,sem_list_names,'bw')
 create_sdm('bw_env.sdm','bw_lex.sdm',outdir,bw_des_lex,lex_list_names,'bw')
-create_sdm('bw_env.sdm','bw_probsem.sdm',outdir,bw_des_probsem,prob_list_names,'bw')
-create_sdm('bw_env.sdm','bw_semdist.sdm',outdir,bw_des_semdist,semdist_list_names,'bw')
 
 
 create_sdm_noenv('bw_env.sdm','bw_full_noenv.sdm',outdir,bw_des_full,full_list_names,'bw')
 create_sdm_noenv('bw_env.sdm','bw_sem_noenv.sdm',outdir,bw_des_sem,sem_list_names,'bw')
 create_sdm_noenv('bw_env.sdm','bw_lex_noenv.sdm',outdir,bw_des_lex,lex_list_names,'bw')
-create_sdm_noenv('bw_env.sdm','bw_probsem_noenv.sdm',outdir,bw_des_probsem,prob_list_names,'bw')
-create_sdm_noenv('bw_env.sdm','bw_semdist_noenv.sdm',outdir,bw_des_semdist,semdist_list_names,'bw')
 
 
 
@@ -162,14 +135,12 @@ create_sdm_noenv('bw_env.sdm','bw_semdist_noenv.sdm',outdir,bw_des_semdist,semdi
 create_mdm('natexp_env.mdm','fw_full.sdm','bw_full.sdm','full_model.mdm',outdir)
 create_mdm('natexp_env.mdm','fw_sem.sdm','bw_sem.sdm','sem_model.mdm',outdir)
 create_mdm('natexp_env.mdm','fw_lex.sdm','bw_lex.sdm','lex_model.mdm',outdir)
-create_mdm('natexp_env.mdm','fw_probsem.sdm','bw_probsem.sdm','probsem_model.mdm',outdir)
-create_mdm('natexp_env.mdm','fw_semdist.sdm','bw_semdist.sdm','24jul_semdist_model.mdm',outdir)
+
 
 create_mdm('natexp_env.mdm','fw_full_noenv.sdm','bw_full_noenv.sdm','full_model_noenv.mdm',outdir)
 create_mdm('natexp_env.mdm','fw_sem_noenv.sdm','bw_sem_noenv.sdm','sem_model_noenv.mdm',outdir)
 create_mdm('natexp_env.mdm','fw_lex_noenv.sdm','bw_lex_noenv.sdm','lex_model_noenv.mdm',outdir)
-create_mdm('natexp_env.mdm','fw_probsem_noenv.sdm','bw_probsem_noenv.sdm','probsem_model_noenv.mdm',outdir)
-create_mdm('natexp_env.mdm','fw_semdist_noenv.sdm','bw_semdist_noenv.sdm','24jul_semdist_model_noenv.mdm',outdir)
+
 
 
 %% Visualization
@@ -191,15 +162,14 @@ disp(corrcoef(bw_des_full))
 
 %% Function for Preds Matrix creation 
 
-function [des_full,des_sem,des_lex,des_semprob,des_semdist]= create_preds(hrf,tt,t,data,fss)
+function [des_full,des_sem,des_lex]= create_preds(hrf,tt,t,data,fss)
 
     ref = zeros(size(hrf));
     ref_ss = zeros(size(hrf));
     ref_ls = zeros(size(hrf));
     ref_fr = zeros(size(hrf));
     ref_env = zeros(size(hrf));
-    ref_sp = zeros(size(hrf));
-    ref_sd = zeros(size(hrf));
+
 
 
     timestamps = data.timestamps;
@@ -214,8 +184,6 @@ function [des_full,des_sem,des_lex,des_semprob,des_semdist]= create_preds(hrf,tt
         ref_ss(iws:iwe) = data.ss(i);
         ref_ls(iws:iwe) = data.ls(i);
         ref_fr(iws:iwe) = data.fr(i);
-        ref_sp(iws:iwe) = data.sp(i);
-        ref_sd(iws:iwe) = data.sd(i);
         ref_env(iws:iwe) = rms(audio(round(iws/100*fss):round(iwe/100*fss))); % RMS signal envelope
 
     end
@@ -226,8 +194,6 @@ function [des_full,des_sem,des_lex,des_semprob,des_semdist]= create_preds(hrf,tt
     ssc = conv(ref_ss,hrf,'full'); ssc = ssc(1:length(tt));
     lsc = conv(ref_ls,hrf,'full'); lsc = lsc(1:length(tt));
     frc = conv(ref_fr,hrf,'full'); frc = frc(1:length(tt));
-    spc = conv(ref_sp,hrf,'full'); spc = spc(1:length(tt));
-    sdc = conv(ref_sd,hrf,'full'); sdc = sdc(1:length(tt));
     envc = conv(ref_env,hrf,'full'); envc = envc(1:length(tt));
     
     %% interpolation
@@ -235,8 +201,6 @@ function [des_full,des_sem,des_lex,des_semprob,des_semdist]= create_preds(hrf,tt
     lsci = interp1(tt,lsc,t);
     frci = interp1(tt,frc,t);
     refci = interp1(tt,refc,t);
-    spci = interp1(tt,spc,t);
-    sdci = interp1(tt,sdc,t);
     envci = interp1(tt,envc,t);
 
     
@@ -244,8 +208,7 @@ function [des_full,des_sem,des_lex,des_semprob,des_semdist]= create_preds(hrf,tt
     %GS orthogonalization
     [des_sem,~] = gsog([ssci',frci',refci',envci']);
     [des_lex,~] = gsog([lsci',frci',refci',envci']);
-    [des_semprob,~] = gsog([spci',frci',refci',envci']);
-    [des_semdist,~] = gsog([sdci',frci',refci',envci']);
+
 
     
 %     des_full = [ssci',lsci',frci',refci',envci'];
@@ -264,7 +227,7 @@ end
 
 function create_sdm(input_name,output_name,outdir,data,list_names,direction)
 
-    sdm = xff([outdir,'\',input_name]);
+    sdm = xff(input_name);
     TotPreds = size(list_names,2);
     sdm.SDMMatrix = zeros(size(data,1),TotPreds);
     sdm.NrOFPredictors = TotPreds;
